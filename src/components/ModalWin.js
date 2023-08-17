f027d211d380fd3864b16831bef451d5912e878c
Pavel-Kliukin <9213087@mail.ru>
2023-08-18 00:29:13 +0300

    ModalWin design and mobile version done

--- src/components/ModalWin.js
+++ src/components/ModalWin.js
@@ -3,6 +3,8 @@ import './ModalWin.css'
 
 // Images imports
 import winGif from '../assets/win.gif'
+import leftClapingHands from '../assets/LClapingHands.gif'
+import rightClapingHands from '../assets/RClapingHands.gif'
 
 const ModalWin = (props) => {
 
@@ -10,20 +12,23 @@ const ModalWin = (props) => {
 
   return (
     <div className='bigBoxWin'>
-      <div className="overlayWin">
-      </div>
+      <div className="overlayWin"></div>
       <div className="modalWin">
-        <h2 id="missioncompleted">Mission completed!</h2>
-        <h2 id="youWin">You win!</h2>
-        <div id="winGifBox">
-          <img id="winGif" src={winGif} alt="WinGif"/>
+        <div className='congrats'>
+          <h2 id="missioncompleted">Mission completed!</h2>
+          <h2 id="youWin">You win!</h2>
+        </div>
+        <img id="winGif" src={winGif} alt="WinGif"/>
+        <div className='buttomBox'>
+          <img className='hands' id="leftClapingHands" src={leftClapingHands} alt="leftClapingHands"/>
+          <form>
+              <label>Enter your name:</label>
+              <input></input>
+            <button>Submit</button>
+          </form>
+          <img className='hands' id="rightClapingHands" src={rightClapingHands} alt="rightClapingHands"/>
         </div>
         <button id="closeButton" className="btn" onClick={props.btnClicked}>x</button>
-        <form>
-          <label>Enter your name</label>
-          <input></input>
-          <button>Submit</button>
-        </form>
       </div>
     </div>
   );
